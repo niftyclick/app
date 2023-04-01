347aaa02ed125953621244f1f9f87cb182f76f5a
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2023-04-01 09:18:50 +0530

    :coffin: removed nft info

--- lib/screens/nft_gallery.dart
+++ lib/screens/nft_gallery.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:nifty_click_app/constants.dart';
 import 'package:http/http.dart' as http;
 import 'package:shimmer/shimmer.dart';
 
@@ -30,15 +28,11 @@ class _NFTGalleryState extends State<NFTGallery> {
         http.Response response = await http.get(
           Uri.parse(
               "https://api-mainnet.magiceden.dev/v2/wallets/${widget.publicKey}/tokens?offset=0&limit=100&listStatus=both"),
-          headers: {
-            "accept": "application/json",
-            "Authorization": "0478fab5-5d5b-4a3f-a5d7-e56cc570dc33",
-          },
         );
         for (var nft in (decoder.convert(response.body) as List)) {
           if (nft != null) {
             if (nft["image"] != "") {
-              nfts.add("${nft["image"]}");
+              nfts.add(nft["image"]);
             }
           }
         }
@@ -62,34 +56,31 @@ class _NFTGalleryState extends State<NFTGallery> {
                   crossAxisSpacing: 16,
                   children: snapshot.data!
                       .map(
-                        (nft) => GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(16),
-                              child: CachedNetworkImage(
-                                imageUrl: nft.toString(),
-                                placeholder: (context, url) {
-                                  return Shimmer.fromColors(
-                                    baseColor:
-                                        const Color.fromRGBO(34, 34, 34, 1),
-                                    highlightColor:
-                                        const Color.fromARGB(255, 55, 58, 63),
-                                    period: const Duration(milliseconds: 850),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color:
-                                            const Color.fromRGBO(34, 34, 34, 1),
-                                        borderRadius: BorderRadius.circular(16),
-                                      ),
+                        (nft) => Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: CachedNetworkImage(
+                              imageUrl: nft.toString(),
+                              placeholder: (context, url) {
+                                return Shimmer.fromColors(
+                                  baseColor:
+                                      const Color.fromRGBO(34, 34, 34, 1),
+                                  highlightColor:
+                                      const Color.fromARGB(255, 55, 58, 63),
+                                  period: const Duration(milliseconds: 850),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color:
+                                          const Color.fromRGBO(34, 34, 34, 1),
+                                      borderRadius: BorderRadius.circular(16),
                                     ),
-                                  );
-                                },
-                              ),
+                                  ),
+                                );
+                              },
                             ),
                           ),
                         ),
